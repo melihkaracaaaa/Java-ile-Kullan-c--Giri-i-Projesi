64357c78d04102d5a8b3453792a52ca7057ac4a5
Melihcan Karaca <karacamelihcan1@gmail.com>
2022-09-05 21:07:37 +0300

    Update KullanıcıGirişiProgramı.java

--- Kullanıcı Girişi Programı/src/kullanıcı/girişi/programı/KullanıcıGirişiProgramı.java
+++ Kullanıcı Girişi Programı/src/kullanıcı/girişi/programı/KullanıcıGirişiProgramı.java
@@ -1,55 +1,55 @@
 
-package kullanıcı.girişi.programı;
+package kullanici.girişi.programi;
 
 import java.util.Scanner;
 
-public class KullanıcıGirişiProgramı {
+public class KullaniciGirişiProgrami {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int Giriş_Hakki = 3;
+        int Giris_Hakki = 3;
         
-        String sys_Kullanıcı_adı = "Melihcan Karaca";
+        String sys_Kullanici_adi = "Melihcan Karaca";
         String sys_Parola = "12345";
         
         System.out.println("********************************");
-        System.out.println("Kullanıcı Girişine Hoşgeldiniz...");
+        System.out.println("Kullanici Girisine Hoşgeldiniz...");
         System.out.println("********************************");
         
         while (true) {
             
-            System.out.print("Kullanıcı Adı :");
+            System.out.print("Kullanici Adi :");
             String kullanici = scanner.nextLine();
             
             System.out.print("Parola :");
             String parola = scanner.nextLine();
             
-            if (kullanici.equals(sys_Kullanıcı_adı) && parola.equals(sys_Parola)){
+            if (kullanici.equals(sys_Kullanici_adi) && parola.equals(sys_Parola)){
             
-                System.out.println("Hoşgeldiniz..." + kullanici);
+                System.out.println("Hosgeldiniz..." + kullanici);
                 break;
             }
             
-            else if (kullanici.equals(sys_Kullanıcı_adı) && !parola.equals(sys_Parola)){
-                System.out.println("Parolanız Yanlış...");
+            else if (kullanici.equals(sys_Kullanici_adı) && !parola.equals(sys_Parola)){
+                System.out.println("Parolaniz Yanlis...");
                 Giriş_Hakki -= 1;
-                System.out.println("Kalan Giriş Hakkı : " + Giriş_Hakki);
+                System.out.println("Kalan Giris Hakki : " + Giris_Hakki);
                 
             }
-            else if (!kullanici.equals(sys_Kullanıcı_adı) && parola.equals(sys_Parola)){
-                System.out.println("Kullanıcı Adınız Yanlış...");
-                Giriş_Hakki -= 1;
-                System.out.println("Kalan Giriş Hakkı : " + Giriş_Hakki);
+            else if (!kullanici.equals(sys_Kullanici_adi) && parola.equals(sys_Parola)){
+                System.out.println("Kullanici Adiniz Yanlis..");
+                Giris_Hakki -= 1;
+                System.out.println("Kalan Giris Hakki : " + Giris_Hakki);
             }
             else {
-                System.out.println("Kullanıcı Adı ve Parolanız Yanlış...");
+                System.out.println("Kullanici Adi ve Parolaniz Yanlis...");
                         
-                Giriş_Hakki -= 1;
-                System.out.println("Kalan Giriş Hakkı : " + Giriş_Hakki);
+                Giris_Hakki -= 1;
+                System.out.println("Kalan Giris Hakki : " + Giris_Hakki);
             }
             
-            if (Giriş_Hakki == 0) {
-                System.out.println("Giriş Hakkınız Bitti. Daha sonra tekrar deneyiniz...");
+            if (Giris_Hakki == 0) {
+                System.out.println("Giris Hakkiniz Bitti. Daha sonra tekrar deneyiniz...");
                 break;
                 
             }
